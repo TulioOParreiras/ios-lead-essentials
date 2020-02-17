d4e42eef12098363792e0d3966a0581d623a1505
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-17 16:59:55 -0300

    Load command delivers no images on empty cache

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -36,7 +36,7 @@ public final class LocalFeedLoader {
             if let error = error {
                 completion(.failure(error))
             } else {
-                
+                completion(.success([]))
             }
         }
     }
