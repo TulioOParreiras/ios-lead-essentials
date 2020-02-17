e0f0a7cc798e5284afc117ef2c3d734752c0d18e
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-17 16:10:13 -0300

    Move `RemoteFeedItem` to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Usemobile on 17/02/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
