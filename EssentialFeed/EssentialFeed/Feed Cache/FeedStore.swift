cd4bd4b9d11df3f5ef071fae2d74b8cfcf987919
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-17 17:19:10 -0300

    Load command delivers cached images on less than seven days old cache

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -8,10 +8,16 @@
 
 import Foundation
 
+public enum RetrieveCacheFeedResult  {
+    case empty
+    case found(feed: [LocalFeedImage], timestamp: Date)
+    case failure(Error)
+}
+
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (Error?) -> Void
+    typealias RetrievalCompletion = (RetrieveCacheFeedResult) -> Void
     
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -32,11 +32,14 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error = error {
-                completion(.failure(error))
-            } else {
-                completion(.success([]))
+        store.retrieve { result in
+            switch result {
+            case let .failure(error):
+            completion(.failure(error))
+            case let .found(feed, timestamp):
+                completion(.success(feed.toModels()))
+            case .empty:
+            completion(.success([]))
             }
         }
     }
@@ -53,5 +56,11 @@ public final class LocalFeedLoader {
 private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
+    }
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
     }
 }
