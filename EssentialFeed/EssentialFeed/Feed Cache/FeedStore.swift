d1c49950441682592813f6285fe9ef3436a1895c
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-17 16:11:03 -0300

    Move `LocalFeedItem` to its own file.

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -16,23 +16,3 @@ public protocol FeedStore {
     func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
 
-
-public struct LocalFeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(
-        id: UUID,
-        description: String?,
-        location: String?,
-        imageURL: URL) {
-        
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-    
-}
---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,29 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Usemobile on 17/02/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(
+        id: UUID,
+        description: String?,
+        location: String?,
+        imageURL: URL) {
+        
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+    
+}
