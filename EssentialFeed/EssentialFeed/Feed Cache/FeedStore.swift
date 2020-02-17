aa416113a4f83de9f350b7e2cc71458c5dc9f216
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-17 16:40:34 -0300

    Load command requests cache retrieval

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -14,5 +14,6 @@ public protocol FeedStore {
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    func retrieve()
 }
 
--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -15,6 +15,7 @@ class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCacheFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -46,5 +47,9 @@ class FeedStoreSpy: FeedStore {
     
     func completeInsertionSuccessfully(at index: Int = 0) {
         insertionCompletions[index](nil)
+    }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
     }
 }
