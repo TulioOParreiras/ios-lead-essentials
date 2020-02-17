496a0a4e6350a8d9ea1557c3adb0c03bc7d9eba6
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-17 11:22:43 -0300

    Move `LocalFeedLoader` (and `FeedStore` collaborator) to its own file at production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,47 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Usemobile on 17/02/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate(), completion: { [weak self] error in
+            guard self != nil else { return }
+            
+            completion(error)
+        })
+    }
+}
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
