242a4e9736f05d49b5cd5c2949a48c86ae992724
Tulio Parreiras <tulio@usemobile.xyz>
2020-02-18 08:59:31 -0300

    Deletes cache on retrieval error

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -36,13 +36,14 @@ public final class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
-            completion(.failure(error))
+                self.store.deleteCachedFeed { _ in }
+                completion(.failure(error))
                 
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
                 
             case .found, .empty:
-            completion(.success([]))
+                completion(.success([]))
             }
         }
     }
--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCachUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCachUseCaseTests.swift
@@ -76,6 +76,15 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCacheFeed])
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
