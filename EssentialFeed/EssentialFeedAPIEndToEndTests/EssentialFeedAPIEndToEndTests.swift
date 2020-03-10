266b10fd13a0bd6833f14d64e1d7fd6e1c5052db
Tulio Parreiras <tulio@usemobile.xyz>
2020-03-10 18:22:47 -0300

    Make `CodableFeedStore` queue concurrent to allow multiple `retrieve` to be processed in parallel (since it has no side-effects) and use `barries` when performing side-effects to guarantee data consistency and avoid race conditions.

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -40,7 +40,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         
-        let exp = expectation(description: "Wair for load completion")
+        let exp = expectation(description: "Wait for load completion")
         
         var receivedResult: LoadFeedResult?
         loader.load { result in
