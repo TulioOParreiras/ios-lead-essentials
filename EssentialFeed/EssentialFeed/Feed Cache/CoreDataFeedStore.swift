f8fdb952f04f4fd65db8c1c77a39d60773498989
Tulio Parreiras <tulio@usemobile.xyz>
2020-03-13 11:32:30 -0300

    Make `storeURL` an explicit dependency so we can inject test-specific URLs (such as `/dev/null`) to avoid sharing state with production (and other tests!)

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -13,8 +13,8 @@ public final class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
-    public init(bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
     }
     
@@ -33,42 +33,44 @@ public final class CoreDataFeedStore: FeedStore {
 }
 
 private extension NSPersistentContainer {
-   enum LoadingError: Swift.Error {
-       case modelNotFound
-       case failedToLoadPersistentStores(Swift.Error)
-   }
-
-   static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
-       guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-           throw LoadingError.modelNotFound
-       }
-
-       let container = NSPersistentContainer(name: name, managedObjectModel: model)
-       var loadError: Swift.Error?
-       container.loadPersistentStores { loadError = $1 }
-       try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-
-       return container
-   }
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let description = NSPersistentStoreDescription(url: url)
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [description]
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
 }
 
 private extension NSManagedObjectModel {
-   static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-       return bundle
-           .url(forResource: name, withExtension: "momd")
-           .flatMap { NSManagedObjectModel(contentsOf: $0) }
-   }
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
 }
 
 private class ManagedCache: NSManagedObject {
-   @NSManaged var timestamp: Date
-   @NSManaged var feed: NSOrderedSet
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
 }
 
 private class ManagedFeedImage: NSManagedObject {
-   @NSManaged var id: UUID
-   @NSManaged var imageDescription: String?
-   @NSManaged var location: String?
-   @NSManaged var url: URL
-   @NSManaged var cache: ManagedCache
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -24,52 +24,53 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-
+        
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-
+        
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
-
+        
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-
+        
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-
+        
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
-
+        
     }
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
-
+        
     }
     
     func test_delete_deliversNoErrorOnNonEmptyCache() {
-
+        
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
-
+        
     }
     
     func test_storeSideEffects_runSerially() {
-
+        
     }
-
+    
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-         let sut = try! CoreDataFeedStore(bundle: storeBundle)
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(bundle: storeBundle, storeURL: storeURL)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-
+    
 }
