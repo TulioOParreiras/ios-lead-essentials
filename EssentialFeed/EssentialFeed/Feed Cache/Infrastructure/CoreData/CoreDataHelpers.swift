06283c0a27d65b2c978a39cc75f66d1679ce6d93
Tulio Parreiras <tulio@usemobile.xyz>
2020-03-19 16:53:28 -0300

    Remove redundant internal access control declarations

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
@@ -8,7 +8,7 @@
 
 import CoreData
 
-internal extension NSPersistentContainer {
+extension NSPersistentContainer {
     enum LoadingError: Swift.Error {
         case modelNotFound
         case failedToLoadPersistentStores(Swift.Error)
