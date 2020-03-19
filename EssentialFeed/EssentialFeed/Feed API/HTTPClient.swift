dfa3767f7e309046a06b20b23187df9e6b17ca78
Tulio Parreiras <tulio@usemobile.xyz>
2020-03-19 17:24:39 -0300

    Replace custom `HTTPClienteResult` enum with a nested typealias over the standard `Swift.Result`

--- EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -8,13 +8,11 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
 
 public protocol HTTPClient {
+
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+    func get(from url: URL, completion: @escaping (Result) -> Void)
 }
--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -168,13 +168,13 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
         var requestedURLs: [URL] {
             messages.map{ $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void ) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void ) {
             messages.append((url, completion))
         }
         
@@ -189,7 +189,7 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                 httpVersion: nil,
                 headerFields: nil)!
             
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
     
