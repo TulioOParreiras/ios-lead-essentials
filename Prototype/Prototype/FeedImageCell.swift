d3cd198431038dbeab3fa5bb35841d7458e46eb8
Tulio Parreiras <tulio@usemobile.xyz>
2020-03-20 10:36:34 -0300

    Add `FeedImageCell` with storyboard connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Usemobile on 20/03/20.
+//  Copyright © 2020 tulioParreiras. All rights reserved.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+    
+}
