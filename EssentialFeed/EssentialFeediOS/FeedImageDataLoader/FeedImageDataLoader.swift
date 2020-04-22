c7b6f6d0422aea4665e3f04bd1d2c8bd7afebb46
Tulio Parreiras <tulio@usemobile.xyz>
2020-04-22 12:56:03 -0300

    Move `FeedImageDataLoader` to its own file

---
+++ EssentialFeed/EssentialFeediOS/FeedImageDataLoader/FeedImageDataLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Usemobile on 22/04/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
