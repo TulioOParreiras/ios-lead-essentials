758fc1b587f56dfa869019366fa2f139bed18d43
Tulio Parreiras <tulio@usemobile.xyz>
2020-04-22 12:42:52 -0300

    Extract UIButton test helper to a separate file

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Usemobile on 22/04/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
+
