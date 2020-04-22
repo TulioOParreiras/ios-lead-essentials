8a9a475f0447e6c1af21549aa47c8a4e2d729143
Tulio Parreiras <tulio@usemobile.xyz>
2020-04-22 12:44:18 -0300

    Extract duplicate logic into a reusable UIControl extension

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -10,11 +10,7 @@ import UIKit
 
 extension UIButton {
     func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
+        simulate(event: .touchUpInside)
     }
 }
 
