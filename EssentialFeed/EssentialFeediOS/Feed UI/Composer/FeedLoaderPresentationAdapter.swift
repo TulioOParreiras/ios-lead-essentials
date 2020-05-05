1b07c40db490003e75c0ad1a805553a1783536e6
Tulio Parreiras <tulio@usemobile.xyz>
2020-05-05 17:44:50 -0300

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,33 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Usemobile on 05/05/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+                
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
+
