d5e1caa13af3cc431071c85ca4039c2e59152aeb
Tulio Parreiras <tulio@usemobile.xyz>
2020-05-05 15:54:02 -0300

    Display image with animation

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Animations.swift
+//  EssentialFeediOS
+//
+//  Created by Usemobile on 05/05/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
+
