0aa1e54baf44b5117e0005274a53f37504a00057
Tulio Parreiras <tulio@usemobile.xyz>
2020-05-06 20:45:46 -0300

    Remove default `URLSessionHTTPClient` initializer parameter (dependency) as clients should be mindful about which `URLSession` instance to use. For example, clients may decide to use the `.shared` instance, but when testing we can inject a `URLSession` with an ephemeral configuration registered with the `URLProtocolStub` class. This way, we don't need to start/stop intercepting URL requests globally.

--- EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -11,7 +11,7 @@ import Foundation
 public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
-    public init(session: URLSession = .shared) {
+    public init(session: URLSession) {
         self.session = session
     }
     
