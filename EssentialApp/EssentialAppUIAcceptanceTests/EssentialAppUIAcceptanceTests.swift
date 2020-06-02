34402babca21c9f95432f66b44644a992be220af
Tulio Parreiras <tulio@usemobile.xyz>
2020-06-01 22:04:21 -0300

    Intercept HTTP requests with canned responses during UI tests to eliminate network fleakiness - We can now run UI tests without internet connection.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,7 +8,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 	
 	func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
 		let app = XCUIApplication()
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
 		app.launch()
 		
 		let feedCells = app.cells.matching(identifier: "feed-image-cell")
@@ -20,7 +20,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 	
 	func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
 		let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
 		onlineApp.launch()
 
 		let offlineApp = XCUIApplication()
