0fae1b4eeb32c4cfb58865f3758704a0ad37f08b
Tulio Parreiras <tulio@usemobile.xyz>
2020-06-12 16:58:01 -0300

    Record feed with failed image loading snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -36,6 +36,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT() -> FeedViewController {
@@ -58,6 +66,19 @@ class FeedSnapshotTests: XCTestCase {
             ImageStub(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
                       location: "Garth Pier",
                       image: UIImage.make(withColor: .green))
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil)
         ]
     }
     
