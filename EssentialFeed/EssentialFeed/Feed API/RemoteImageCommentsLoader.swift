7a5c781bef1fa368bc7882b451e95411fdbe2cb1
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-11-27 15:17:58 -0300

    Implement ImageComment mapping

--- EssentialFeed/EssentialFeed/Feed API/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteImageCommentsLoader.swift
@@ -17,7 +17,7 @@ public final class RemoteImageCommentsLoader {
         case invalidData
     }
     
-    public typealias Result = FeedLoader.Result
+    public typealias Result = Swift.Result<[ImageComment], Swift.Error>
     
     public init(url: URL, client: HTTPClient) {
         self.url = url
@@ -41,16 +41,10 @@ public final class RemoteImageCommentsLoader {
     private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
         do {
             let items = try ImageCommentsMapper.map(data, from: response)
-            return .success(items.toModels())
+            return .success(items)
         } catch {
             return .failure(error)
         }
-    }
-}
-
-private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedImage] {
-        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
     }
 }
 
