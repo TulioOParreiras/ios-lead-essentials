c66362d3c41b61e506d0f11a5dccf63d6ca419fc
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-11-28 10:41:20 -0300

    Rename and move ResourceErrorView and ViewModel to Shared module

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -9,7 +9,7 @@ public protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, FeedErrorView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
 	public var delegate: FeedViewControllerDelegate?
 	@IBOutlet private(set) public var errorView: ErrorView?
     
@@ -44,7 +44,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 		refreshControl?.update(isRefreshing: viewModel.isLoading)
 	}
 	
-	public func display(_ viewModel: FeedErrorViewModel) {
+	public func display(_ viewModel: ResourceErrorViewModel) {
 		errorView?.message = viewModel.message
 	}
 
