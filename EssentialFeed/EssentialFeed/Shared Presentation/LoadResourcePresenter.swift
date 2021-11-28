7115f05ceb30123ef8de4d9dba5c617759dba2f2
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-11-28 10:42:19 -0300

    Use correct bundle to load generic connection error

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -24,7 +24,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     public static var loadError: String {
         return NSLocalizedString("GENERIC_CONNECTION_ERROR",
                 tableName: "Shared",
-                bundle: Bundle(for: FeedPresenter.self),
+                bundle: Bundle(for: Self.self),
                 comment: "Error message displayed when we can't load the resource from the server")
     }
     
