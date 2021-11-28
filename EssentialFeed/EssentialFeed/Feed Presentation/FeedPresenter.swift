d3c610c2fc48444c8e49265fb1244f5a4d0cdbca
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-11-28 10:59:02 -0300

    Remove unused FeedPresenter logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -4,48 +4,12 @@
 
 import Foundation
 
-public protocol FeedView {
-	func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
-	private let feedView: FeedView
-	private let loadingView: ResourceLoadingView
-	private let errorView: ResourceErrorView
-	
-	private var loadError: String {
-		return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-				tableName: "Shared",
-				bundle: Bundle(for: Self.self),
-				comment: "Error message displayed when we can't load the resource from the server")
-	}
-	
-	public init(feedView: FeedView, loadingView: ResourceLoadingView, errorView: ResourceErrorView) {
-		self.feedView = feedView
-		self.loadingView = loadingView
-		self.errorView = errorView
-	}
-	
 	public static var title: String {
 		return NSLocalizedString("FEED_VIEW_TITLE",
 			tableName: "Feed",
 			bundle: Bundle(for: FeedPresenter.self),
 			comment: "Title for the feed view")
-	}
-	
-	public func didStartLoadingFeed() {
-		errorView.display(.noError)
-		loadingView.display(ResourceLoadingViewModel(isLoading: true))
-	}
-	
-	public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-		loadingView.display(ResourceLoadingViewModel(isLoading: false))
-	}
-	
-	public func didFinishLoadingFeed(with error: Error) {
-		errorView.display(.error(message: loadError))
-		loadingView.display(ResourceLoadingViewModel(isLoading: false))
 	}
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
