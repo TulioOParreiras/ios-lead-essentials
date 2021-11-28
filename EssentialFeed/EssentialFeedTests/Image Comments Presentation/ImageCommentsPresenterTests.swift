2e235b15eded724ad6c3b546a6e52ce4c11892ca
Tulio Parreiras <tulio.parreiras-ext@ab-inbev.com>
2021-11-28 12:51:02 -0300

    Localize image comments title

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -11,7 +11,19 @@ import EssentialFeed
 class ImageCommentsPresenterTests: XCTestCase {
     
     func test_title_isLocalized() {
-        XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+        XCTAssertEqual(ImageCommentsPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
+    
+    // MARK: - Helpers
+
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
     }
     
 }
